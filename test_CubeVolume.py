44259c7c7a78d610e6f414c17b4051b78e18b1ba
borngasp <56008554+borngasp@users.noreply.github.com>
2021-02-07 18:46:57 -0800

    Added comments to test

--- test_CubeVolume.py
+++ test_CubeVolume.py
@@ -3,20 +3,24 @@ import CubeVolume
 
 class CubeVolumeTestCases(unittest.TestCase):
 
+    #A valid int test
     def test1(self):
         result = CubeVolume.volume(2)
         self.assertEqual(result, 8)
 
+    #An invalid string value for x
     def test2(self):
         result = CubeVolume.volume("help")
         #print(result)
         self.assertEqual(result, "Not a valid cube")
 
+    #An invalid negative sized cube
     def test3(self):
         result = CubeVolume.volume(-2)
         #print(result)
         self.assertEqual(result, "Not a valid cube")
 
+    #A valid float sized cube
     def test4(self):
         result = CubeVolume.volume(12.34)
         self.assertEqual(result,1879.080904)
